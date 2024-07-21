8526872c24730b40fc8ee9a446ee6a92c2343652
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-07-21 15:24:15 +0330

    Create Product Template

--- products/views.py
+++ products/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Products
 
 
 def index(request):
-    return HttpResponse('')
+    products = Products.objects.all()
+    return render(request,
+                  'index.html',
+                  {'products': products})
 
 
 def new_products(request):
