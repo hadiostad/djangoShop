eaee4a35835a2e6318f0a449ec4f8944dd400e03
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-07-21 12:19:40 +0330

    creat new products views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('')
+
+
+def new_products(request):
+    return HttpResponse('new products')
+
+
