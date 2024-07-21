9944f55b81cc6751818295e1ee84c09c560e7e9c
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-07-21 12:53:01 +0330

    create product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# create product model
+class Products(models.Model):
+    name = models.CharField(max_length=255 )
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
