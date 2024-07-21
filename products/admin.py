d0300c1fbac6dab58e525b90412aab19df8b41f5
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-07-21 14:02:56 +0330

    add product to admin panel

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Products
 
 
-admin.site.register(Products)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Products, ProductAdmin)
