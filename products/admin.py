acbaf31cc096e87b2c2b62762273a27184a52422
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-07-21 14:16:43 +0330

    Add Discount To Admin Panel

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Discount
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock')
 
 
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
 admin.site.register(Products, ProductAdmin)
+admin.site.register(Discount, DiscountAdmin)
