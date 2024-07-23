fac429359f7563faf9b603821152a5c05d0a367c
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-07-23 21:20:36 +0330

    creat css file and fix image views

--- djangoShop/settings.py
+++ djangoShop/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
